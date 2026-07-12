bab6800bc574095d646911682f03f70238beaa36
Ken Kato <kkato149@gmail.com>
2026-07-12 14:15:37 +0900

    [API] Remove Foo filed from custom resource Password

--- api/v1alpha1/password_types.go
+++ api/v1alpha1/password_types.go
@@ -30,9 +30,6 @@ type PasswordSpec struct {
 	// The following markers will use OpenAPI v3 schema to validate the value
 	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
 
-	// foo is an example field of Password. Edit password_types.go to remove/update
-	// +optional
-	Foo *string `json:"foo,omitempty"`
 }
 
 // PasswordStatus defines the observed state of Password.
